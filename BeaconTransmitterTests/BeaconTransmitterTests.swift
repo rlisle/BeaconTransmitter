914412d65c7ed57f13031c903c6fc14f32ed9cff
Ron Lisle <ron@lisles.net>
2017-07-04 07:55:48 -0500

    Initial files

--- BeaconTransmitterTests/BeaconTransmitterTests.swift
+++ BeaconTransmitterTests/BeaconTransmitterTests.swift
@@ -9,28 +9,18 @@
 import XCTest
 @testable import BeaconTransmitter
 
-class BeaconTransmitterTests: XCTestCase {
+class BeaconTransmitterTests: XCTestCase
+{
+    var beacon: BeaconTransmitter!
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        beacon = BeaconTransmitter()
     }
+
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testBeaconInstantiates() {
+        XCTAssertNotNil(beacon)
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
