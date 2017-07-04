bcc859d049ab42b0825e45f4aa4c9a10437856a3
Ron Lisle <ron@lisles.net>
2017-07-04 07:52:02 -0500

    Initial Commit

---
+++ BeaconTransmitter/BeaconTransmitter.h
@@ -0,0 +1,19 @@
+//
+//  BeaconTransmitter.h
+//  BeaconTransmitter
+//
+//  Created by Ron Lisle on 7/4/17.
+//  Copyright © 2017 Ron Lisle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BeaconTransmitter.
+FOUNDATION_EXPORT double BeaconTransmitterVersionNumber;
+
+//! Project version string for BeaconTransmitter.
+FOUNDATION_EXPORT const unsigned char BeaconTransmitterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BeaconTransmitter/PublicHeader.h>
+
+
